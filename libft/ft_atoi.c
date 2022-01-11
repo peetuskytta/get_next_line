4a0f5c5e36fae0acd50451a8edaf8c23bfb584e5
pskytta <peetu.skytta@gmail.com>
2022-01-11 19:04:40 +0200

    Added the Libft project library

---
+++ libft/ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pskytta <pskytta@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 16:53:14 by pskytta           #+#    #+#             */
+/*   Updated: 2021/12/20 17:01:43 by pskytta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	unsigned int	i;
+	long			sign;
+	long			result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (ft_is_wspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
