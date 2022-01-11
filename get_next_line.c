0a20ce2ff0112ebca63db231d2d9a8688a93b0e6
pskytta <peetu.skytta@gmail.com>
2022-01-11 18:59:37 +0200

    first commit

---
+++ get_next_line.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pskytta <pskytta@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 15:25:28 by pskytta           #+#    #+#             */
+/*   Updated: 2022/01/10 14:24:59 by pskytta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/*
+	Function get_next_line will read a file and return a line read.
+*/
+
+int	get_next_line(const int fd, char **line)
+{
+}
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pskytta <pskytta@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 14:30:33 by pskytta           #+#    #+#             */
+/*   Updated: 2022/01/10 14:30:57 by pskytta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+# define FD_SIZE 4096
+# define BUFF_SIZE 8192
+# include "libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+int	get_next_line(const int fd, char **line);
+
+#endif
