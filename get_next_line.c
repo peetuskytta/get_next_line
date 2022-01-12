47396b657b9c50042123db25aa4596e1beb391c9
pskytta <peetu.skytta@gmail.com>
2022-01-12 22:49:10 +0200

    Testing hard

--- get_next_line.c
+++ get_next_line.c
@@ -11,8 +11,72 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
+
+static int	join_line(char **line, char **str)
+{
+	size_t	length;
+	char	*temp;
+
+	length = 0;
+	while ((*str)[length] != '\n' && (*str)[length] != '\0')
+		length++;
+	if ((*str)[length] == '\n')
+	{
+		*line = ft_strsub(*str, 0, length);
+		temp = ft_strdup(&((*str)[length + 1]));
+		free(*str);
+		*str = temp;
+		if ((*str)[0] == '\0')
+		{
+			ft_strdel(str);
+		}
+	}
+	else
+	{
+		*line = ft_strdup(*str);
+		ft_strdel(str);
+	}
+	return (1);
+}
+
+static int	check_ret_value(int fd, char **line, int ret, char **str)
+{
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0 && str[fd] == NULL)
+	{
+		return (0);
+		printf("EOF reached\n");
+	}
+	else
+		return (join_line(line, &str[fd]));
+}
 
 int	get_next_line(const int fd, char **line)
 {
-
+	static char		*str[FD_SIZE];
+	char			*temp;
+	char			buffer[BUFF_SIZE + 1];
+	int				ret;
+	
+	if (fd < 0 || line == NULL)
+		return (-1);
+	ret = read(fd, buffer, BUFF_SIZE);
+	while (ret > 0)
+	{
+		buffer[ret] = '\0';
+		if (str[fd] == NULL)
+			str[fd] = ft_strdup(buffer);
+		else
+		{
+			temp = ft_strjoin(str[fd], buffer);
+			free(str[fd]);
+			str[fd] = temp;
+		}
+		if (ft_strchr(str[fd], '\n'))
+			break ;
+		ret = read(fd, &buffer, BUFF_SIZE);
+	}
+	return (check_ret_value(fd, line, ret, str));
 }
--- main.c
+++ main.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/11 19:20:39 by marvin            #+#    #+#             */
-/*   Updated: 2022/01/11 19:20:39 by marvin           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 #include <stdio.h>
 
@@ -17,15 +5,43 @@ int main()
 {
   char  *line;
   int   fd1;
+  
+    fd1 = open("file1", O_RDONLY);
+    printf("fd1 value: %d\n", fd1);
+    get_next_line(fd1, &line);
+    printf("\n------------------------------------");
+    printf("\nin main\n");
+    printf("returned line 1 in main: \t%s\n", line);
+    printf("------------------------------------");
+    printf("\n\n");
+    get_next_line(fd1, &line);
+    printf("\n------------------------------------");
+    printf("\nin main\n");
+    printf("returned line 2 in main: \t%s\n", line);
+    printf("------------------------------------");
+    printf("\n\n");
+    get_next_line(fd1, &line);
+    printf("\n------------------------------------");
+    printf("\nin main\n");
+    printf("returned line 3 in main: \t%s\n", line);
+    printf("------------------------------------");
+    printf("\n\n");
+    get_next_line(fd1, &line);
+    printf("\n------------------------------------");
+    printf("\nin main\n");
+    printf("returned line 4 in main: \t%s\n", line);
+    printf("------------------------------------");
+    printf("\n\n");
 
-  fd1 = open("file1", O_RDONLY);
-  get_next_line(fd1, &line);
-  printf("%s\n", line);
-/*  get_next_line(fd1, &line);
-  printf("%s\n", line);
-  get_next_line(fd1, &line);
-  printf("%s\n", line);
-  get_next_line(fd1, &line);
-  printf("%s\n", line);*/
+        get_next_line(fd1, &line);
+    printf("\n------------------------------------");
+    printf("\nin main\n");
+    printf("returned EOF: \t%s\n", line);
+    printf("------------------------------------");
+    printf("\n\n");
+
+    printf("fd1: %d", fd1);
+
+    close(fd1);
   return (0);
 }
