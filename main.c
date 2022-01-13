96f8ccfc26f18d95b32b0c1015b8471f92559a2d
pskytta <peetu.skytta@gmail.com>
2022-01-13 16:36:46 +0200

    Testing with one file only

--- main.c
+++ main.c
@@ -4,44 +4,44 @@
 int main()
 {
   char  *line;
-  int   fd1;
+  int   fd;
+  int   ret;
+  int   i;
+
+  ret = 1;
+  i = 0;
+  fd = open("file1", O_RDONLY);
+
+  printf("\n");
+  printf("\033[0;34m------------------------------------\033[0m\n");
+
+  while (ret == 1)
+  {
+    ret = get_next_line(fd, &line);
+    if (ret == 0)
+      break ;
+    printf("line %d:\t\033[0;33m%s\033[0m", i, line);
+    printf("\n\033[0;34m------------------------------------\033[0m\n");
+    i++;
+  }
+  if (ret == 0)
+  {
+    printf("ret value: \033[0;32m%d\033[0m\t", ret);
+    printf("Status:\033[0;32m\tEOF reached\033[0m\n");
+    printf("\033[0;34m------------------------------------\033[0m\n\n");
+  }
+  else if (ret == -1)
+  {
+    printf("\nret value: \033[0;31m%d\033[0m\t", ret);
+    printf("Status:\033[0;31m\tError\033[0m\n\n");
+    printf("\n\033[0;34m------------------------------------\033[0m\n\n");
+  }
+  /*else
+  {
+    printf("\nret value: \033[0;32m%d\033[0m\t", ret);
+    printf("\n\033[0;34m------------------------------------\033[0m\n\n");
+  }*/
   
-    fd1 = open("file1", O_RDONLY);
-    printf("fd1 value: %d\n", fd1);
-    get_next_line(fd1, &line);
-    printf("\n------------------------------------");
-    printf("\nin main\n");
-    printf("returned line 1 in main: \t%s\n", line);
-    printf("------------------------------------");
-    printf("\n\n");
-    get_next_line(fd1, &line);
-    printf("\n------------------------------------");
-    printf("\nin main\n");
-    printf("returned line 2 in main: \t%s\n", line);
-    printf("------------------------------------");
-    printf("\n\n");
-    get_next_line(fd1, &line);
-    printf("\n------------------------------------");
-    printf("\nin main\n");
-    printf("returned line 3 in main: \t%s\n", line);
-    printf("------------------------------------");
-    printf("\n\n");
-    get_next_line(fd1, &line);
-    printf("\n------------------------------------");
-    printf("\nin main\n");
-    printf("returned line 4 in main: \t%s\n", line);
-    printf("------------------------------------");
-    printf("\n\n");
-
-        get_next_line(fd1, &line);
-    printf("\n------------------------------------");
-    printf("\nin main\n");
-    printf("returned EOF: \t%s\n", line);
-    printf("------------------------------------");
-    printf("\n\n");
-
-    printf("fd1: %d", fd1);
-
-    close(fd1);
+  close(fd);
   return (0);
 }
