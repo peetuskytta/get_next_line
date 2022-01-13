3b22d15da9fc6b6b308f402a94a4275fc69b6e2e
pskytta <peetu.skytta@gmail.com>
2022-01-13 19:47:00 +0200

    Tested with 2 files

--- main.c
+++ main.c
@@ -1,47 +1,50 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-int main()
+int main(int ac, char **av)
 {
   char  *line;
   int   fd;
   int   ret;
-  int   i;
+  int   line_count;
+  int   k;
 
-  ret = 1;
-  i = 0;
-  fd = open("file1", O_RDONLY);
-
+  k = 1;
   printf("\n");
   printf("\033[0;34m------------------------------------\033[0m\n");
-
-  while (ret == 1)
+  while (k <= ac)
   {
-    ret = get_next_line(fd, &line);
-    if (ret == 0)
-      break ;
-    printf("line %d:\t\033[0;33m%s\033[0m", i, line);
-    printf("\n\033[0;34m------------------------------------\033[0m\n");
-    i++;
+    line_count = 1;
+    ret = 1;
+    fd = open(av[k], O_RDONLY);
+    while (ret == 1)
+    {
+      if (fd == -1)
+        return (0);
+      ret = get_next_line(fd, &line);
+      if (ret == 0)
+      {
+        printf("ret value: \033[0;32m%d\033[0m\t", ret);
+        printf("Status:\033[0;32m\tEOF reached\033[0m\n");
+        printf("\033[0;34m------------------------------------\033[0m\n\n");
+        break ;
+      }
+      else if (ret == -1)
+      {
+        printf("\nret value: \033[0;31m%d\033[0m\t", ret);
+        printf("Status:\033[0;31m\tError\033[0m\n");
+        printf("\n\033[0;34m------------------------------------\033[0m\n");
+      }
+      else
+      {
+        printf("line %d:\t\033[0;33m%s\033[0m", line_count, line);
+        printf("\n\033[0;34m------------------------------------\033[0m\n");
+        ++line_count;
+      }
+    }
+    ++k;
   }
-  if (ret == 0)
-  {
-    printf("ret value: \033[0;32m%d\033[0m\t", ret);
-    printf("Status:\033[0;32m\tEOF reached\033[0m\n");
-    printf("\033[0;34m------------------------------------\033[0m\n\n");
-  }
-  else if (ret == -1)
-  {
-    printf("\nret value: \033[0;31m%d\033[0m\t", ret);
-    printf("Status:\033[0;31m\tError\033[0m\n\n");
-    printf("\n\033[0;34m------------------------------------\033[0m\n\n");
-  }
-  /*else
-  {
-    printf("\nret value: \033[0;32m%d\033[0m\t", ret);
-    printf("\n\033[0;34m------------------------------------\033[0m\n\n");
-  }*/
-  
   close(fd);
+  printf("\n");
   return (0);
 }
